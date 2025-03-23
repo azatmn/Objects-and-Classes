a5ed88dc3d289f4ad0dd6b4d2ea3a8d2ed4f2644
Azat <minyazovazat@gmail.com>
2025-03-23 20:48:15 +0300

    Task 2

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Implemented two child classes Lecturer and Reviewer from the parent Mentor class
+Implemented two child classes Lecturer and Reviewer from the parent Mentor class.
+
+Now only the Reviewer can give grades to the Student. And a Student can give grades to a Lecturer, but only if the Lecturer is assigned to the course to which the Student is attached. I also added examples.
--- main.py
+++ main.py
@@ -7,6 +7,15 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def RatingForLecturer(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress:
+            if course in lecturer.courses_attached:
+                lecturer.courses_attached[course] += [grade]
+            else:
+                lecturer.courses_attached[course] = [grade]
+        else:
+            return "Ошибка"
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -14,6 +23,14 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.courses_attached = {}
+
+
+class Reviewer (Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -24,22 +41,19 @@ class Mentor:
             return 'Ошибка'
 
 
-class Lecturer (Mentor):
-    pass
-
-
-class Reviewer (Mentor):
-    pass
-
-
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 
-cool_mentor = Mentor('Some', 'Buddy')
+cool_mentor = Reviewer('Some', 'Buddy')
 cool_mentor.courses_attached += ['Python']
 
 cool_mentor.rate_hw(best_student, 'Python', 10)
 cool_mentor.rate_hw(best_student, 'Python', 10)
 cool_mentor.rate_hw(best_student, 'Python', 10)
 
-print(best_student.grades)
+print(f"{best_student.name} {best_student.surname}'s grades: {best_student.grades}")
+
+good_lecturer = Lecturer("Ray", "Ban")
+best_student.RatingForLecturer(good_lecturer, 'Python', 10)
+best_student.RatingForLecturer(good_lecturer, 'Python', 10)
+print(f"{good_lecturer.name} {good_lecturer.surname}'s grades: {good_lecturer.courses_attached}")
