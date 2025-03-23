5ce43caea97c3bac706c35e6a270bb23b3e164fc
Azat <minyazovazat@gmail.com>
2025-03-23 22:32:46 +0300

    Task 3

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Implemented two child classes Lecturer and Reviewer from the parent Mentor class.
 
 Now only the Reviewer can give grades to the Student. And a Student can give grades to a Lecturer, but only if the Lecturer is assigned to the course to which the Student is attached. I also added examples.
+
+Overloaded the magic method __str__ for all classes. Implemented the opportunity to compare lecturers and students according to average grades. Gave examples
--- main.py
+++ main.py
@@ -16,6 +16,29 @@ class Student:
         else:
             return "Ошибка"
 
+    def calculate_average_grade(self):
+        total_sum = 0
+        total_count = 0
+        for course_grades in self.grades.values():
+            total_sum += sum(course_grades)
+            total_count += len(course_grades)
+
+        if total_count == 0:
+            return 0
+        return total_sum / total_count
+
+    def __str__(self):
+        return (f"Имя: {self.name} \nФамилия: {self.surname} \n"
+                f"Средняя оценка за домашние задания: {self.calculate_average_grade()} \n"
+                f"Курсы в процессе изучения: {', '.join(self.courses_in_progress)} \n"
+                f"Завершенные курсы: {', '.join(self.finished_courses)} \n")
+
+    def __lt__(self, other):
+        if self.calculate_average_grade() < other.calculate_average_grade():
+            return f"{other.name} {other.surname}"
+        else:
+            return f"{self.name} {self.surname}"
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -29,6 +52,27 @@ class Lecturer(Mentor):
         super().__init__(name, surname)
         self.courses_attached = {}
 
+    def calculate_average_grade(self):
+        total_sum = 0
+        total_count = 0
+        for course_grades in self.courses_attached.values():
+            total_sum += sum(course_grades)
+            total_count += len(course_grades)
+        if total_count == 0:
+            return 0
+        return total_sum / total_count
+
+    def __str__(self):
+        return (f"Имя: {self.name} \n"
+                f"Фамилия: {self.surname} \n"
+                f"Средняя оценка за лекции: {self.calculate_average_grade()} \n")
+
+    def __lt__(self, other):
+        if self.calculate_average_grade() < other.calculate_average_grade():
+            return f"{other.name} {other.surname}"
+        else:
+            return f"{self.name} {self.surname}"
+
 
 class Reviewer (Mentor):
     def rate_hw(self, student, course, grade):
@@ -40,20 +84,42 @@ class Reviewer (Mentor):
         else:
             return 'Ошибка'
 
+    def __str__(self):
+        return (f"Имя: {self.name} \n"
+                f"Фамилия: {self.surname} \n")
 
-best_student = Student('Ruoy', 'Eman', 'your_gender')
+
+best_student = Student('Ruoy', 'Eman', 'man')
 best_student.courses_in_progress += ['Python']
+best_student.courses_in_progress += ['Git']
+best_student.finished_courses += ['Введение в программирование']
+
+other_student = Student('Steve', 'Jobs', 'man')
+other_student.courses_in_progress += ['Python']
+other_student.finished_courses += ['Введение в программирование']
 
 cool_mentor = Reviewer('Some', 'Buddy')
 cool_mentor.courses_attached += ['Python']
+cool_mentor.courses_attached += ['Git']
+
 
 cool_mentor.rate_hw(best_student, 'Python', 10)
 cool_mentor.rate_hw(best_student, 'Python', 10)
 cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, "Git", 9)
+cool_mentor.rate_hw(other_student, "Python", 10)
 
-print(f"{best_student.name} {best_student.surname}'s grades: {best_student.grades}")
 
 good_lecturer = Lecturer("Ray", "Ban")
 best_student.RatingForLecturer(good_lecturer, 'Python', 10)
-best_student.RatingForLecturer(good_lecturer, 'Python', 10)
-print(f"{good_lecturer.name} {good_lecturer.surname}'s grades: {good_lecturer.courses_attached}")
+best_student.RatingForLecturer(good_lecturer, 'Python', 7)
+
+other_lecturer = Lecturer("Mark", "Zuckerberg")
+other_student.RatingForLecturer(other_lecturer, 'Python', 10)
+
+for i in (best_student, other_student, good_lecturer, other_lecturer, cool_mentor):
+    print(i)
+
+print(f"Лучший студент: {best_student < other_student}\n ")
+
+print(f"Лучший лектор: {good_lecturer < other_lecturer}")
